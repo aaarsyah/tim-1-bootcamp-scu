488759062cc310975a1f4344736bb4967678b9e4
Addinda Ayu Arsyah <addinda.dev@gmail.com>
2025-09-24 01:40:10 +0700

    add implementation data binding on login and page register

--- default-project-main/src/08.BlazorUI/obj/Debug/net9.0/MyApp.BlazorUI.AssemblyInfo.cs
+++ default-project-main/src/08.BlazorUI/obj/Debug/net9.0/MyApp.BlazorUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670fc889c9106014f6eb96831b4e8f581d7b8477")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
