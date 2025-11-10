aa4fa2c4100fb3ce7fb4ffcb1c09ee96b24a0e74
RealOfficialTurf <59943769+RealOfficialTurf@users.noreply.github.com>
2025-11-10 00:09:46 +0000

    Updated sonar-scan.bat

--- sonar-scan.bat
+++ sonar-scan.bat
@@ -1,10 +1,13 @@
+REM set token disini
+set /A TOKEN=sqp_0123456789abcdef0123456789abcdef01234567
+
 REM pre-run cleanup
 dotnet clean MyApp.sln
 
 dotnet sonarscanner begin ^
 /k:"BootcampProject" ^
 /d:sonar.host.url="http://localhost:9000" ^
-/d:sonar.token="sqp_18e64cfeee3aa8f4e04c29aec052a87c7b074577" ^
+/d:sonar.token="%TOKEN%" ^
 /d:sonar.cs.opencover.reportsPaths="TestResults/**/coverage.opencover.xml" ^
 /d:sonar.cs.vstest.reportsPaths="TestResults/**/*.trx" ^
 /d:sonar.coverage.exclusions="**/Migrations/**,**/wwwroot/**,**/*.cshtml,**/Program.cs" ^
@@ -17,7 +20,7 @@ dotnet test MyApp.sln --configuration Release --no-build --logger "trx" --result
 --collect:"XPlat Code Coverage" ^
 -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
 
-dotnet sonarscanner end /d:sonar.token="sqp_18e64cfeee3aa8f4e04c29aec052a87c7b074577"
+dotnet sonarscanner end /d:sonar.token="%TOKEN%"
 
 REM post-run cleanup
 rmdir TestResults /S /Q
